979fa3468d03e8915246c229a1abf085a52a1c72
상민 박 <smpark95@mindslab.ai>
2021-05-19 11:32:39 -0400

    Add basic styling and structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav/Nav';
+import Shop from './components/Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
+  let path = window.location.pathname;
+  path = path.split('/');
+  path = path[path.length - 1];
+
+  const routes = [{ url: '/', component: Dashboard }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Nav />
+        <Switch>
+          <div className='content'>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/shop' component={Shop} />
+          </div>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
